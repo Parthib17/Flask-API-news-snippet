1bfb0b3ce4203e649ed3ab10bf9052bd3b2a67ed
Parthib17 <parthibmitra1278@gmail.com>
2024-08-28 00:03:49 +0530

    first commin

---
+++ app.py
@@ -0,0 +1,39 @@
+import pickle
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Define the summary function
+import nltk
+nltk.download('punkt')
+def summary(url):
+
+    from newspaper import Article
+
+    # Download the punkt tokenizer
+
+
+    try:
+        article = Article(url)
+        article.download()
+        article.parse()
+        article.nlp()
+        return article.summary
+    except Exception as e:
+        return f"An error occurred: {e}"
+
+# Load the pickled model
+model = pickle.load(open('summary_function.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return "hello world"
+
+@app.route('/summarize', methods=['POST'])
+def summarize():
+    url = request.form.get('url')
+    result = { 'summary': model(url)}
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
