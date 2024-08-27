1b3c55c8847e318a1d1f406c4191ecfdb84a68a1
Parthib17 <parthibmitra1278@gmail.com>
2024-08-28 01:26:07 +0530

    fifth committ

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='your_project_name',
+    version='1.0',
+    install_requires=[
+        # List of your dependencies here, if any
+    ],
+    scripts=['nltk_download.py'],
+)
